25d66e016ac98e659668cc2075ec880494e1305d
felipe <feliperestrepomeneses@gmail.com>
2021-11-03 23:55:26 -0500

    Agreamos geometric.js

---
+++ geometric.js
@@ -0,0 +1,8 @@
+//Figuras geometricas.
+
+// Tenemos varias figuras geométricas, entre ellas el cuadrado, que tiene un perímetro, es lo que mide todo el borde del cuadrado, que serian por ejemplo 4 lados iguales, de 5cm cada uno. Es decir que nuestro perímetro seria de 20cm.
+// Luego tenemos el área, que no es una medida de una sola dimensión, son dos dimensiones. Es decir que tendremos que multiplicar uno de los lados por otro de los lados. 5x5 = 25cm2 de área.
+// Ahora con el triangulo, tenemos muchos tipos, pero suponiendo que tenemos uno cuyos lados miden 6cm y cuya base mide 4cm, diríamos que el perímetro es de 16cm. Para calcular el área tendremos que calcular base por altura, recordando que la base ya la tenemos y la altura, supongamos que es de 5.5cm. Asi que para sacar el área tendremos que multiplicar Base por Altura Dividido 2 (B * A) / 2, 
+
+// Para los círculos, tendremos que lo que llamamos perímetro, en realidad es la circunferencia. Para ello primero necesitaremos el radio, que es la línea que parte desde la mitad hasta el borde del circulo. Y tendremos el diámetro que es el radio * 2. Para calcular la circunferencia hay que multiplicar nuestro diámetro por pi (diámetro * PI). Para calcular el área del circulo será (Radio^2 * PI)
+
