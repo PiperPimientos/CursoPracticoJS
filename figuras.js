b1dbe3b75ad0868a8542793cdc11f77b873e6593
felipe <feliperestrepomeneses@gmail.com>
2021-12-04 23:27:46 -0500

    Haremos el reto del triangulo isosceles

--- figuras.js
+++ figuras.js
@@ -233,3 +233,19 @@ function calcularAreaCuadrado() {
     const area = areaCuadrado(value);
     alert(area);
 }
+
+
+
+// Reto: matemáticas con JavaScript
+// 9/24
+
+// LECTURA
+
+// Durante este taller aprendimos a calcular el área y perímetro de un triángulo conociendo la longitud de sus 3 lados y también su altura.
+
+// En este ejercicio debes crear una función para calcular la altura de un triángulo isósceles.
+
+// La función debe recibir, como parámetros, la longitud de los 3 lados del triángulo.
+// La función debe validar que la longitud de los 3 lados del triángulo corresponden a un triángulo isósceles.
+// La función debe retornar la altura del triángulo.
+// Pista: la función Math.sqrt de JavaScript puede ayudarte a calcular raíces cuadradas.
